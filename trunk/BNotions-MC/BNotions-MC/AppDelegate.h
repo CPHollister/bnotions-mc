752c6bbfb6eed0541a7eaef28ec422b307c4d94a
Kyle Brooks <kyle@brksy.com>
2013-07-15 19:24:50 -0400

    Created initial xcode project

---
+++ trunk/BNotions-MC/BNotions-MC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BNotions-MC
+//
+//  Created by Kyle Brooks on 2013-07-15.
+//  Copyright (c) 2013 Kyle Brooks Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ trunk/BNotions-MC/BNotions-MC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BNotions-MC
+//
+//  Created by Kyle Brooks on 2013-07-15.
+//  Copyright (c) 2013 Kyle Brooks Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
